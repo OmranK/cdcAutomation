1f891673f30deb1ed502b5efe7b76dc4fd650e6c
Omran Khoja <omran@artimislabs.com>
2025-02-07 20:43:31 -0500

    Guard to check properties are all there before doing automation

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const filePath = './input.csv';
 // Define the object structure
 class Person {
     constructor(uuid, sex, ageyears, height, weight) {
-      this.uuid = uuid;
+      this.uuid = uuid
       this.sex = sex
       this.age = ageyears
       this.height = height
@@ -42,47 +42,51 @@ fs.createReadStream(filePath)
     await page.setViewport({width: 1512, height: 982, isMobile: false, hasTouch: false, deviceScaleFactor: 1});
     await page.goto("https://www.cdc.gov/bmi/child-teen-calculator/index.html");
     for (const person of people) {
-        // console.log("Attempting to parse - " + row);
-        await page.waitForSelector('#metric_units_radio', {visible: true});
-        await page.click('#metric_units_radio');
-        if (person.sex === "1") {
-            await page.waitForSelector('#sex_boy_radio');
-            await page.click('#sex_boy_radio');
-        } else if (person.sex === "2") {
-            await page.waitForSelector('#sex_girl_radio');
-            await page.click('#sex_girl_radio');
-        }
+        if (!person.height, !person.weight, !person.age, !person.sex) {
+            person.healthy = "error";
+            appendPersonToCSV(person)
+        } else {
+            await page.waitForSelector('#metric_units_radio', {visible: true});
+            await page.click('#metric_units_radio');
+            if (person.sex === "1") {
+                await page.waitForSelector('#sex_boy_radio');
+                await page.click('#sex_boy_radio');
+            } else if (person.sex === "2") {
+                await page.waitForSelector('#sex_girl_radio');
+                await page.click('#sex_girl_radio');
+            }
 
-        await page.waitForSelector('#age_years_input');
-        await page.type('#age_years_input', person.age);
-        await page.waitForSelector('#height_cm_input');
-        await page.type('#height_cm_input', person.height);
-        await page.waitForSelector('#weight_kg_input');
-        await page.type('#weight_kg_input', person.weight);
-        await page.waitForSelector('#calculate_btn');
-        await page.click('#calculate_btn');
-        await page.waitForSelector('#calc_results', {visible: true});
-        let element = await page.waitForSelector('.bmi_category', {visible: true});
-        let bmiCategory = await element.evaluate(el => el.textContent);
-        switch (bmiCategory) {
-            case "Healthy Weight":
-                person.healthy = "1"
-                break;
-            case "Obesity":
-                person.obesity = "1"
-                break;
-            case "Overweight":
-                person.overweight = "1"
-                break;
-            case "Underweight":
-                person.underweight = "1"
-                break;
-            case "Severe Obesity":
-                person.severeObesity = "1"
-                break;
+            await page.waitForSelector('#age_years_input');
+            await page.type('#age_years_input', person.age);
+            await page.waitForSelector('#height_cm_input');
+            await page.type('#height_cm_input', person.height);
+            await page.waitForSelector('#weight_kg_input');
+            await page.type('#weight_kg_input', person.weight);
+            await page.waitForSelector('#calculate_btn');
+            await page.click('#calculate_btn');
+            await page.waitForSelector('#calc_results', {visible: true});
+            let element = await page.waitForSelector('.bmi_category', {visible: true});
+            let bmiCategory = await element.evaluate(el => el.textContent);
+            switch (bmiCategory) {
+                case "Healthy Weight":
+                    person.healthy = "1"
+                    break;
+                case "Obesity":
+                    person.obesity = "1"
+                    break;
+                case "Overweight":
+                    person.overweight = "1"
+                    break;
+                case "Underweight":
+                    person.underweight = "1"
+                    break;
+                case "Severe Obesity":
+                    person.severeObesity = "1"
+                    break;
+            }
+            appendPersonToCSV(person)
+            await page.click('.back_btn');
         }
-        appendPersonToCSV(person)
-        await page.click('.back_btn');
     }
     await browser.close();
 })();
